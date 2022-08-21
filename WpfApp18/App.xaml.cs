2eb9155e5cac63cae2fbcb5f18aed37b9063878b
lllypik <blesphoton@gmail.com>
2022-08-21 15:20:56 +0300

    Добавьте файлы проекта.

---
+++ WpfApp18/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WpfApp18
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
